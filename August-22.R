387a7ffbf01cbc4978fceb4232b7168e47c3c011
1savion <savion.brown@icloud.com>
2024-08-22 23:11:02 -0400

    Add files via upload
    This is Savion Brown class assignment from August 22nd 2024

---
+++ August-22.R
@@ -0,0 +1,17 @@
+# Author: Savion Brown, Date: 08-22-2024, Purpose: Test the measures of descriptive stats
+
+# How to calculate standard deviation in R
+
+
+X <- c(1,2,3,4,5)
+sd(X)
+
+# c container holds the numbers in the variabke or X
+
+Y <- 100
+
+# How to calculate the mean in R mean()
+# For gaussian distribution st()
+# For median do median()
+
+# mode is only used using a custom function 
