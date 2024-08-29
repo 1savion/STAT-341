d9b6446a0a29979f1629bda3f79811b0815f4e02
1savion <savion.brown@icloud.com>
2024-08-29 16:06:23 -0400

    Assignment3.R
    August 29

---
+++ Assignment3-R.R
@@ -0,0 +1,10 @@
+variable1 <- sample(x = 1:10, size = 1000)
+
+variable1 <- sample(x = 1:10, size = 1000, replace = TRUE)
+> sd(variable1)
+[1] 2.930937
+> mean(variable1)
+[1] 5.486
+> median(variable1)
+[1] 5.5
+> 
